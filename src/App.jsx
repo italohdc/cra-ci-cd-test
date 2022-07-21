f27f00a6c5f95370d4ee9e01d45eabb2f34ddb3e
Italo Lelis <italo.carvalho@natahouse.com>
2022-07-21 14:09:39 -0300

    update html

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        <p className='vermelho'>Vermelho</p>
+        <p className='verde'>Verde</p>
+        <p className='azul'>Azul</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
